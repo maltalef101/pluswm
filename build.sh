88b0d83889c0c5ed59b3d48b3873912afdb1763b
Manuel Altalef <maltalef@auror.ar>
2021-05-30 17:32:36 -0300

    feat(distro detection): installs dependencies
    The modified build script now checks for what distro you are running and
    installs certain dependencies depending, no pun intended, on that.

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+printf "$0: Checking for dependencies... \n"
+
 printf "$0: Looking for build directory... \n"
 
 [ -d ./build ] || mkdir ./build
@@ -9,3 +11,40 @@ cmake .. -G Ninja
 printf "$0: Building... \n"
 ninja && printf "$0: All done! Built binary is in \`build/\`\n"
 cd ..
+
+installDeps() {
+	distro=$(whatDistro)
+	if [ $distro = "debian" ]; then
+		isInstalled "xorg libx11-dev cmake ninja-build libgoogle-glog-dev g++"
+	elif [ $distro = "arch" ]; then
+		isInstalled "xorg libx11 cmake ninja google-glog gcc"
+	else;
+		printf "$0: Distribution could not be identified! Please install the dependencies listed in the README.md file."
+		exit 1
+	fi
+}
+
+isInstalled() {
+	distro=$(whatDistro)
+
+	packagesNotInstalled=""
+
+	if [ $distro = "debian" ]; then
+		for i in "$1"; do
+			[ apt list | grep "^$i.*installed" ] || printf "$0: $i is not installed!\n"; $packagesNotInstalled=$packagesNotInstalled"$i "
+		done
+		apt install $packagesNotInstalled
+	fi
+}
+
+whatDistro() {
+	osRelease="$(cat /etc/os-release | awk -F= '/^NAME/ {print $2}')"
+
+	if [ $osRelease = "Debian GNU/Linux" ]; then
+		return "debian"
+	elif [ $osRelease = "Arch Linux" ]; then
+		return "arch"
+	else
+		return "unknown"
+	fi
+}
