38a67f6c89eae0c492b62d7789a41254474ce48c
Manuel Altalef <maltalef@auror.ar>
2021-06-01 02:04:11 -0300

    build script: dont remember

--- build.sh
+++ build.sh
@@ -1,29 +1,18 @@
 archlinux=$(type pacman >/dev/null 2>/dev/null; echo $?)
 debian=$(type apt >/dev/null 2>/dev/null; echo $?)
 
-isInstalled() {
-	packagesNotInstalled=""
-
-	if [ $debian ]; then
-		for i in "$1"; do
-			apt list | grep "^$i.*installed" || printf "$0: $i is not installed!\n"; $packagesNotInstalled=$packagesNotInstalled"$i "
-		done
-		apt install $packagesNotInstalled
-	fi
-}
-
 installDeps() {
-	if [ $debian ] ; then
-		isInstalled "xorg libx11-dev cmake ninja-build libgoogle-glog-dev g++"
-	elif [ $archlinux ] ; then
-		isInstalled "xorg libx11 cmake ninja google-glog gcc"
+	if [ $debian -eq 0 ] ; then
+		sudo apt install xorg libx11-dev cmake ninja-build libgoogle-glog-dev g++
+	elif [ $archlinux -eq 0 ] ; then
+		sudo pacman -S --needed xorg-server libx11 cmake ninja google-glog gcc
 	else
 		printf "$0: Distribution could not be identified! Please install the dependencies listed in the README.md file.\n"
 		exit 1
 	fi
 }
 
-printf "$0: Checking for dependencies... \n"
+printf "$0: Installing dependencies... \n"
 installDeps
 
 printf "$0: Looking for build directory... \n"
