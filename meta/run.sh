697f2cc04ef6383d6b4cb4e7f7be9dcee6519413
Manuel Altalef <maltalef@auror.ar>
2022-04-25 12:23:20 -0300

    meta/run: Xephyr doesn't work anymore, use Xnest

--- meta/run.sh
+++ meta/run.sh
@@ -2,11 +2,8 @@ set -e
 
 xinitrc_loc="$(git rev-parse --show-toplevel)/meta/xinitrc"
 
-XEPHYR=$(whereis -b Xephyr | cut -f2 -d' ')
+XNEST=$(whereis -b Xnest | cut -f2 -d' ')
 xinit $xinitrc_loc -- \
-    "$XEPHYR" \
+    "$XNEST" \
         :100 \
         -ac \
-        -screen 800x600 \
-        -host-cursor
-
